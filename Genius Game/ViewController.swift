9f66a2b9e99d41dfb6ad3f9def0cb54f057c17b6
Jorge Matias <juniormathiias@gmail.com>
2021-07-09 17:13:51 +0100

    My first commit using some images and creating the button, bckg.

--- Genius Game/ViewController.swift
+++ Genius Game/ViewController.swift
@@ -8,12 +8,18 @@
 import UIKit
 
 class ViewController: UIViewController {
+    @IBOutlet weak var diceToRoll: UIImageView!
+    
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    @IBAction func buttonToRoll(_ sender: UIButton) {
+        let rollNumber = [ #imageLiteral(resourceName: "dice1") , #imageLiteral(resourceName: "dice2") , #imageLiteral(resourceName: "dice3") , #imageLiteral(resourceName: "dice4") , #imageLiteral(resourceName: "dice5") , #imageLiteral(resourceName: "dice6") ]
+        
+        diceToRoll.image = rollNumber[Int.random(in: 0...5)]
+        
+        if rollNumber[2] == rollNumber[3] {
+         print(rollNumber)
+        }
     }
-
-
+    
 }
 
