5b3eb6c0f6b0d12b4271b94b80db58d16b54c6ad
Jorge Matias <juniormathiias@gmail.com>
2021-07-09 15:29:02 +0100

    Initial Commit

---
+++ Genius Game/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Genius Game
+//
+//  Created by Junior Mathias on 09/07/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
